a4a27bde0595a8f12ee7e90b84519da3602f6d31
jasonyang <jasonyangshadow@gmail.com>
2017-06-20 19:11:45 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INSTALLED_PACKAGE=/tmp/installed_package
-BASE=(yaourt xorg xorg-xinit arandr sysstat git gcc gdb neovim zsh tmux python3 python-pip i3-gaps i3exit lightdm pulseaudio networkmanager network-manager-applet bluez blueman xfce4-terminal rofi xarchiver unrar lxappearance nitrogen ranger pcmanfm gparted htop) 
+BASE=(yaourt xorg xorg-xinit arandr sysstat lm_sensors pamac git gcc gdb neovim zsh tmux python3 python-pip i3-gaps i3exit lightdm pulseaudio networkmanager network-manager-applet bluez blueman xfce4-terminal rofi xarchiver unrar lxappearance nitrogen ranger pcmanfm gparted htop) 
 BASE_ADD=(unclutter redshift vlc-nightly cmake viewnior mupdf markdown zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps ibus ibus-kkc ibus-pinyin xfce4-power-manager texlive-most inkscape isousb hexchat)
 SOFTWARES=(powerline-fonts-git oh-my-zsh-git google-chrome-stable thunderbird slack-desktop xfce4-terminal-base16-colors-git uget tor-browser filezilla xmind visual-paradigm-community wps-office ttf-wps-fonts ttf-ms-fonts paper-icon-theme)
 pacman -Qe > $INSTALLED_PACKAGE 
@@ -52,6 +52,7 @@ main(){
             install "${BASE_ADD[*]}" "pacman"
             echo "---------------------copy i3 exit file to /usr/bin/ folder-------------------"
             cp ./i3exit /usr/bin/i3exit
+            cp ./.blur_lock.sh.bak /usr/bin/blurlock
             ;;
         software )
             install "${SOFTWARES[*]}" "yaourt"
@@ -66,7 +67,6 @@ main(){
             cp ./.xinitrc.bak ~/.xinitrc
             cp ./.extend.xinitrc.bak ~/.extend.xinitrc
             cp ./.autostart.sh.bak ~/.autostart.sh
-            cp ./.blur_lock.sh.bak ~/.blur_lock.sh
             cp ./.cronjob.sh.bak ~/.cronjob.sh
             cp ./.tmux.conf.bak ~/.tmux.conf
             cp ./.Xresources.bak ~/.Xresources
