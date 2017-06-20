abe507a7ee3863b90e7be4ef17297602043bfb11
jasonyang <jasonyangshadow@gmail.com>
2017-06-20 14:21:33 +0900

    "update"

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INSTALLED_PACKAGE=/tmp/installed_package
-BASE=(yaourt xorg xorg-xinit arandr sysstat git gcc gdb neovim zsh tmux python3 python-pip i3-gaps i3exit lightdm pulseaudio networkmanager network-manager-applet bluez blueman xfce4-terminal rofi xarchiver unrar lxappearance nitrogen ranger pcmanfm gparted htop unclutter redshift vlc-nightly cmake viewnior mupdf markdown zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps ibus ibus-kkc ibus-pinyin xfce4-power-manager texlive-most inkscape) 
+BASE=(yaourt xorg xorg-xinit arandr sysstat git gcc gdb neovim zsh tmux python3 python-pip i3-gaps i3exit lightdm pulseaudio networkmanager network-manager-applet bluez blueman xfce4-terminal rofi xarchiver unrar lxappearance nitrogen ranger pcmanfm gparted htop unclutter redshift vlc-nightly cmake viewnior mupdf markdown zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps ibus ibus-kkc ibus-pinyin xfce4-power-manager texlive-most inkscape isousb hexchat) 
 SOFTWARES=(powerline-fonts-git oh-my-zsh-git google-chrome-stable thunderbird slack-desktop xfce4-terminal-base16-colors-git uget tor-browser filezilla xmind visual-paradigm-community wps-office ttf-wps-fonts ttf-ms-fonts)
 pacman -Qe > $INSTALLED_PACKAGE 
 
