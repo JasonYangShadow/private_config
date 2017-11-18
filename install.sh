c0b65ea2eb17b7b44e53a428ad12ee24ae7aa1cd
jason <jasonyang.final@gmail.com>
2017-11-18 17:10:36 +0900

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 INSTALLED_PACKAGE=~/installed_package
 BASE=(arandr sysstat lm_sensors acpi acpid git gdb base-devel neovim zsh tmux python3 python-pip python2-pip pulseaudio networkmanager network-manager-applet dhclient xfce4-terminal rofi xarchiver unrar lxappearance nitrogen ranger pcmanfm gparted htop gvfs exfat-utils xdotool xdgutils dmraid dmidecode dosfstools iptables ipw2100-fw ipw2200-fw aic94xx-firmware wd719x-firmware linux-firmware nfs-3g nfs-utils gnome-keyring polkit-gnome tmux rfkill openssh python-virtualenv) 
-BASE_ADD=(unclutter redshift vlc cmake viewnior markdown mupdf zathura zathura-cb zathura-djvu zathura-ps zathura-pdf-mupdf fcitx fcitx-gtk3 fcitx-configtool fcitx-googlepinyin fcitx-kkc xfce4-power-manager isousb cronie shadowsocks proxychains-ng tor arm dnsutils xclip compton lua xscreensaver)
+BASE_ADD=(unclutter redshift vlc viewnior markdown mupdf zathura zathura-cb zathura-djvu zathura-ps zathura-pdf-mupdf fcitx fcitx-gtk3 fcitx-configtool fcitx-googlepinyin fcitx-kkc xfce4-power-manager isousb cronie shadowsocks proxychains-ng tor arm dnsutils xclip compton lua xscreensaver)
 SOFTWARES=(chromium thunderbird slack-desktop xfce4-terminal-base16-colors-git uget filezilla wps-office ttf-wps-fonts ttf-ms-fonts ctags telegram-desktop-bin latex-beamer nodejs npm nodejs-hexo-cli neofetch texlive-most inkscape wqy-microhei docker teamviewer qt4 simplescreenrecorder gimp zeal wireshark-gtk goldendict dropbox megasync)
+mkdir -p ~/installed_package
 pacman -Qe|awk 'BEGIN{FS=" "};{print $1}' > $INSTALLED_PACKAGE 
 
 install(){
